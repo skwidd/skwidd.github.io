8b708d4a36f16530f6ddb03ef4cec55b0a025662
skwidd <kcalamari7@gmail.com>
2025-03-12 11:21:21 -0500

    date fix2

--- script.js
+++ script.js
@@ -14,11 +14,11 @@ fetch("movies.json")
 
 function startNewGame() {
     const today = new Date().toISOString().split("T")[0] // Get today's date in YYYY-MM-DD format
-    currentMovie = movies.find(movies => movies.date === today) || movies[Math.floor(Math.random() * movies.length)];
+    currentMovie = movies.find(({date}) => movies.date === today) || movies[Math.floor(Math.random() * movies.length)];
     clueIndex = 0;
     document.getElementById("rating").textContent = currentMovie.rating;
     //document.getElementById("movie").textContent = currentMovie.title;
-    document.getElementById("date").textContent = currentMovie.date;
+    document.getElementById("date").textContent = ${ currentMovie.date };
     document.getElementById("today").textContent = today;
     document.getElementById("clue0").textContent = `${currentMovie.clues[clueOrder[clueIndex]]}`;
     document.getElementById("message").textContent = "";
