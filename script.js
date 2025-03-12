2b07e6e3078c5b474d86cfd876d804ffd77d6a41
skwidd <kcalamari7@gmail.com>
2025-03-12 09:33:18 -0500

    initial structure setup
    css,js,json,html

---
+++ script.js
@@ -0,0 +1,34 @@
+﻿let movies;
+let currentMovie;
+let clueIndex = 0;
+
+// Load movies from JSON file
+fetch('movies.json')
+    .then(response => response.json())
+    .then(data => {
+        movies = data;
+        startNewGame();
+    });
+
+function startNewGame() {
+    currentMovie = movies[Math.floor(Math.random() * movies.length)];
+    clueIndex = 0;
+    document.getElementById("clue").textContent = currentMovie.clues[clueIndex];
+    document.getElementById("message").textContent = "";
+    document.getElementById("guessInput").value = "";
+}
+
+function checkGuess() {
+    let userGuess = document.getElementById("guessInput").value.trim().toLowerCase();
+    if (userGuess === currentMovie.title.toLowerCase()) {
+        document.getElementById("message").textContent = "Correct! The movie was " + currentMovie.title + "!";
+    } else {
+        clueIndex++;
+        if (clueIndex < currentMovie.clues.length) {
+            document.getElementById("clue").textContent = currentMovie.clues[clueIndex];
+        } else {
+            document.getElementById("message").textContent = "Out of clues! The movie was " + currentMovie.title + ". Try again!";
+            setTimeout(startNewGame, 2000);
+        }
+    }
+}
