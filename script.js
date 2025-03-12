264014e319954efb6e6624e74e7a5910fa4ccf26
skwidd <kcalamari7@gmail.com>
2025-03-12 12:37:10 -0500

    43

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 ﻿let clueIndex = 0
 const clueOrder = ["Profanity", "Sex", "Violence", "Drugs", "Intense Scene"]
 function startNewGame() {
-    var movies = require('movies.json');
+    const movies = require('./movies.json');
     const today = new Date().toISOString().split("T")[0] // Get today's date in YYYY-MM-DD format
     document.getElementById("message").textContent = movies[0].title;
     currentMovie = movies.find(movies => movies.date === today);
