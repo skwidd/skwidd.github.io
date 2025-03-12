0bca6b276305e6d00e316388b9567ae78791e61f
skwidd <kcalamari7@gmail.com>
2025-03-12 13:55:51 -0500

    first build

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 ﻿let clueIndex = 0
+let movies;
+
+fetch('./movies.json')
+.then(res => res.json())
+.then(data => {
+    console.log(data);
+    movies = data;
+});
+
 const clueOrder = ["Profanity", "Sex", "Violence", "Drugs", "Intense Scene"]
 function startNewGame() {
-    import movies from ('./movies.json');
     const today = new Date().toISOString().split("T")[0] // Get today's date in YYYY-MM-DD format
-    document.getElementById("message").textContent = movies[0].title;
-    currentMovie = movies.find(movies => movies.date === today);
+    const currentMovie = movies.find(movies => movies.date === today);
+    //document.getElementById("message").textContent = movies[0].title;
+    console.log(currentMovie.title);
     clueIndex = 0;
     document.getElementById("rating").textContent = currentMovie.rating;
     document.getElementById("year").textContent = currentMovie.year;
-    document.getElementById("runtime").textContent = currentMovie.runtime;
+    document.getElementById("fbombs").textContent = currentMovie.fbombs;
     document.getElementById("clue0").textContent = `${currentMovie.clues[clueOrder[clueIndex]]}`;
     document.getElementById("message").textContent = "";
     document.getElementById("guessInput").value = "";
