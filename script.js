de34be5f0da6b0e0038958ab9938a64287da0039
skwidd <kcalamari7@gmail.com>
2025-03-12 11:43:32 -0500

    56

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ var movies = require("/movies.json");
 
 function startNewGame() {
     const today = new Date().toISOString().split("T")[0] // Get today's date in YYYY-MM-DD format
-    document.getElementById("today").textContent = today;
+    //document.getElementById("today").textContent = today;
 
     currentMovie = movies.find(movies => movies.date === today);
-    if (!currentMovie) {
-        document.getElementById("date").textContent = "movie not found";
-    } else {
-        document.getElementById("date").textContent = currentMovie.date;
-    }
+    //if (!currentMovie) {
+    //    document.getElementById("date").textContent = "movie not found";
+    //} else {
+    //    document.getElementById("date").textContent = currentMovie.date;
+    //}
     clueIndex = 0;
     document.getElementById("rating").textContent = currentMovie.rating;
     //document.getElementById("movie").textContent = currentMovie.title;
@@ -28,7 +28,7 @@ function checkGuess() {
         .getElementById("guessInput")
         .value.trim()
         .toLowerCase()
-    document.getElementById("date").textContent = userGuess + " " + currentMovie.title.toLowerCase();
+    //document.getElementById("date").textContent = userGuess + " " + currentMovie.title.toLowerCase();
     if (userGuess === currentMovie.title.toLowerCase()) {
         document.getElementById("message").textContent =
             "Correct! The movie was " + currentMovie.title + "!"
@@ -36,7 +36,7 @@ function checkGuess() {
         clueIndex++
         if (clueIndex < clueOrder.length) {
             document.getElementById("clue" + clueIndex).textContent =
-                `${clueOrder[clueIndex]}: ${currentMovie.clues[clueOrder[clueIndex]]}`
+                `${currentMovie.clues[clueOrder[clueIndex]]}`
         } else {
             document.getElementById("message").textContent =
                 "Out of clues! The movie was " + currentMovie.title + ". Try again!"
